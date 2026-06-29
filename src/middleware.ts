a94d0354d1cdc3212e3e139991ffd62c957860ac
TekDark <gtrespana@gmail.com>
2026-06-28 22:24:49 -0400

    fix: update middleware matcher to exclude route.ts files

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,6 +27,6 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next|_vercel|.*\\..*).*)',
+    '/((?!api|_next|_vercel|.*\\..*|.*route\\.ts$).*)',
   ],
 }
